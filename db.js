46aef765d3dbc9cbe585c7df8d3c7994e7d0946b
Carlos <carlos.perez2590@gmail.com>
2016-07-16 12:11:35 -0600

    no message

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
   'url' : 'mongodb://heroku_9gv89pgx:a123456@ds023245.mlab.com:23245/heroku_9gv89pgx',
-   'url_test' : 'mongodb://localhost/passport'
+  'url_test' : 'mongodb://localhost/passport'
 }
