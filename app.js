fb17ef0d350e76cf1a51ba58058c7ed89338350b
Carlos2590 <carlos.perez2590@gmail.com>
2016-07-25 22:56:27 -0600

    cambio a express generator

--- app.js
+++ app.js
@@ -1,41 +1,134 @@
-var express = require ('express')
-var fs = require('fs');
-var app = express();
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var dbConfig = require('./db.js');
 var mongoose = require('mongoose');
- var User = require('./models/user.js');
+var mongo = require('mongodb');
+var User = require('./models/user.js');
 //var bCrypt = require('bcrypt');
 
-	 
-	mongoose.connect(dbConfig.url);
+//init app
+var app = express();
 
-	app.use(express.static(__dirname + '/public'));
-	app.get('/', function(peticion, respuesta){
-	
-	respuesta.render('index.jade');
+    mongoose.connect(dbConfig.url);
+    //mongoose.connect('mongodb://localhost/loginapp');
 
 
-var newUser1 = new User();
-// set the user's local credentials
-newUser1.userlongname = "Juan Ernesto";
-newUser1.password = "12345";
-newUser1.email = "miemail@gmail.com";
-// save the user
-newUser1.save(function(err) {
-if (err){
-console.log('No se pudo guardar el usuario: '+err);
-throw err;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+//bodyparser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+
+
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
-console.log('Se registró correctamente el usuario');
-console.log('prueba');
-console.log('prueba carlos');
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
-});
-
-
-
-app.listen(3000, function(){
-	console.log('escuchando en el puerto 3000');
-});
+module.exports = app;
--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
   'url' : 'mongodb://admin:123456@ds023245.mlab.com:23245/heroku_9gv89pgx',
-  'url_test' : 'mongodb://localhost/passport'
+  'url_test' : 'mongodb://localhost/loginapp'
 }
